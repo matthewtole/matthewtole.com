1a46694f3b20d3a0ff26ce2f1b0f71c124d8c804
Matthew Tole <mtole@dropbox.com>
2019-10-16 16:53:02 -0700

    :bug: Fix instagram script to create folders
    Also made it only download the images if needed.

--- src/_data/instagram.js
+++ src/_data/instagram.js
@@ -1,11 +1,17 @@
 const instagram = require("user-instagram");
 const download = require('download');
 const fs = require('fs');
+const mkdirp = require('mkdirp');
 
 module.exports = async () => {
+  mkdirp.sync('_site/static/images/instagram/');
   const data = await instagram("https://www.instagram.com/matthewtole");
   for (let post of data.posts) {
-    await download(post.picture.url).pipe(fs.createWriteStream('_site/static/images/instagram/' + post.shortcode + '.png'));
+    const filename = `_site/static/images/instagram/${post.shortcode}.png`;
+    if (fs.existsSync(filename)) {
+      continue;
+    }
+    await download(post.picture.url).pipe(fs.createWriteStream(filename));
   }
   return data.posts;
 }
