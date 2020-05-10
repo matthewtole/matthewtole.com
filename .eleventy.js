7c1cd53219477a7c842402522b18c570262f4eec
Matthew Tole <matthewtole@gmail.com>
2020-05-09 17:20:17 -0700

    :zap: Fix the non-website project images

--- .eleventy.js
+++ .eleventy.js
@@ -35,6 +35,36 @@ module.exports = eleventyConfig => {
 
   eleventyConfig.addFilter('emojify', emoji.emojify);
 
+  eleventyConfig.addNunjucksAsyncShortcode('responsiveImage', async function(
+    src,
+    alt
+  ) {
+    const options = {
+      outputDir: '_site/img/',
+      cacheDuration: '1w',
+      widths: [256, 512, 1024],
+    };
+    let stats = await Image(src, options);
+    let lowestSrc = stats.jpeg[0];
+
+    let sources = [];
+    Object.values(stats).forEach(imageFormat => {
+      sources.push(
+        `<source type="image/${imageFormat[0].format}" srcset="${imageFormat[0].url}, ${imageFormat[1].url} 2x" media="(min-width: 768px)">`
+      );
+      sources.push(
+        `<source type="image/${imageFormat[0].format}" srcset="${imageFormat[1].url}, ${imageFormat[2].url} 2x">`
+      );
+    });
+
+    return `<picture>${sources.join('\n')}
+      <img
+      class="absolute top-0 left-0 object-cover w-full h-full
+        alt="${alt}" loading="lazy"
+        src="${lowestSrc.url}">
+    </picture>`;
+  });
+
   eleventyConfig.addNunjucksAsyncShortcode('websiteScreenshot', async function(
     url,
     alt
