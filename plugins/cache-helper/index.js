64b4b17990352d3723cf31c1ebb53159b9b58058
Matthew Tole <matthewtole@gmail.com>
2020-05-14 15:17:24 -0700

    Blah

--- plugins/cache-helper/index.js
+++ plugins/cache-helper/index.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
+const cacheDirectories = (constants) => [
+  path.normalize(`${constants.BUILD_DIR}/../.cache`),
+];
+
 module.exports = {
   async onPreBuild({ constants, utils }) {
-    await utils.cache.restore([path.normalize(`${constants.BUILD_DIR}/../.cache`)]);
-    console.log(fs.readdirSync(path.normalize(`${constants.BUILD_DIR}/../.cache`)));
+    await utils.cache.restore(cacheDirectories(constants));
   },
   async onPostBuild({ constants, utils }) {
-    await utils.cache.save([path.normalize(`${constants.BUILD_DIR}/../.cache`)]);
+    await utils.cache.save(cacheDirectories(constants));
   },
 };
