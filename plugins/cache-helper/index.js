084c10207d7ffaf312998af63afe4993594bf007
Matthew Tole <matthewtole@gmail.com>
2020-05-14 15:14:38 -0700

    Tes4

--- plugins/cache-helper/index.js
+++ plugins/cache-helper/index.js
@@ -3,25 +3,10 @@ const path = require('path');
 
 module.exports = {
   async onPreBuild({ constants, utils }) {
-    // const cacheDirs = getCacheDirs(constants);
-
-    // if (await utils.cache.restore(cacheDirs)) {
-    //   console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️');
-    // } else {
-    //   console.log('No Gatsby cache found. Building fresh.');
-    // }
+    await utils.cache.restore([path.normalize(`${constants.BUILD_DIR}/../.cache`)]);
+    console.log(fs.readdirSync(path.normalize(`${constants.BUILD_DIR}/../.cache`)));
   },
   async onPostBuild({ constants, utils }) {
-    // console.log(fs.readdirSync(constants.BUILD_DIR));
-    console.log(fs.readdirSync(path.normalize(`${constants.BUILD_DIR}/../.cache`)));
-    // console.log(fs.readdirSync(path.normalize(`${constants.PUBLISH_DIR}/../`)));
-    // console.log(fs.readdirSync(path.join(constants.CACHE_DIR, '.cache')));
-    // const cacheDirs = getCacheDirs(constants);
-
-    // if (await utils.cache.save(cacheDirs)) {
-    //   console.log('Stored the Gatsby cache to speed up future builds.');
-    // } else {
-    //   console.log('No Gatsby build found.');
-    // }
+    await utils.cache.save([path.normalize(`${constants.BUILD_DIR}/../.cache`)]);
   },
 };
