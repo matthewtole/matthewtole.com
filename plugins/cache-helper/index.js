b9bac5b7c170a25a8c72115af4bfd22b76ba52cc
Matthew Tole <matthewtole@gmail.com>
2020-05-14 15:05:30 -0700

    Test

--- plugins/cache-helper/index.js
+++ plugins/cache-helper/index.js
@@ -1,5 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-  onPreBuild: (constants) => {
-    console.log(constants);
+  async onPreBuild({ constants, utils }) {
+    // const cacheDirs = getCacheDirs(constants);
+
+    // if (await utils.cache.restore(cacheDirs)) {
+    //   console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️');
+    // } else {
+    //   console.log('No Gatsby cache found. Building fresh.');
+    // }
+  },
+  async onPostBuild({ constants, utils }) {
+    console.log(fs.readdirSync(constants.BUILD_DIR));
+    console.log(fs.readdirSync(path.normalize(`${constants.CACHE_DIR}/../`)));
+    console.log(fs.readdirSync(constants.CACHE_DIR));
+    // const cacheDirs = getCacheDirs(constants);
+
+    // if (await utils.cache.save(cacheDirs)) {
+    //   console.log('Stored the Gatsby cache to speed up future builds.');
+    // } else {
+    //   console.log('No Gatsby build found.');
+    // }
   },
 };
